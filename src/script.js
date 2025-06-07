d9fc6c8b1c9d01f18ab565ad223424eb0afc6c58
shubhamaryal <aryalshubham62@gmail.com>
2025-06-07 22:52:10 +0545

    added lights and ghosts using lights

--- src/script.js
+++ src/script.js
@@ -252,13 +252,26 @@ for(let i = 0; i < 30; i++) {
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight("#ffffff", 0.5);
+const ambientLight = new THREE.AmbientLight("#86cdff", 0.275);
 scene.add(ambientLight);
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight("#ffffff", 1.5);
+const directionalLight = new THREE.DirectionalLight("#86cdff", 1);
 directionalLight.position.set(3, 2, -8);
 scene.add(directionalLight);
+
+// Door Light
+const doorLight = new THREE.PointLight("#ff7d46", 5)
+doorLight.position.set(0, 2.2, 2.5);
+house.add(doorLight);
+
+/**
+ * Ghosts
+ */
+const ghost1 = new THREE.PointLight("#ff00ff", 6);
+const ghost2 = new THREE.PointLight("#00ffff", 6);
+const ghost3 = new THREE.PointLight("#ffff00", 6);
+scene.add(ghost1, ghost2, ghost3);
 
 /**
  * Sizes
@@ -319,6 +332,22 @@ const tick = () => {
   // Timer
   timer.update();
   const elapsedTime = timer.getElapsed();
+
+  // Ghosts
+  const ghost1Angle = elapsedTime * 0.5
+  ghost1.position.x = Math.cos(ghost1Angle) * 4;
+  ghost1.position.z = Math.sin(ghost1Angle) * 4;
+  ghost1.position.y = Math.sin(ghost1Angle) * Math.sin(ghost1Angle * 2.34) * Math.sin(ghost1Angle * 3.45)
+
+  const ghost2Angle = -elapsedTime * 0.38
+  ghost2.position.x = Math.cos(ghost2Angle) * 5;
+  ghost2.position.z = Math.sin(ghost2Angle) * 5;
+  ghost2.position.y = Math.sin(ghost2Angle) * Math.sin(ghost2Angle * 2.34) * Math.sin(ghost2Angle * 3.45)
+
+  const ghost3Angle = elapsedTime * 0.23
+  ghost3.position.x = Math.cos(ghost3Angle) * 6
+  ghost3.position.z = Math.sin(ghost3Angle) * 6;
+  ghost3.position.y = Math.sin(ghost3Angle) * Math.sin(ghost3Angle * 2.34) * Math.sin(ghost3Angle * 3.45)
 
   // Update controls
   controls.update();
